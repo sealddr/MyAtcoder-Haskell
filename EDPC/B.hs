c6ba9423671c2587bfd113ffd373e42e5d9a5926
masakazu.kadoshita <mkadoshita@gmail.com>
2022-05-13 10:55:09 +0900

    accelerated EDPC Problem B AC

--- EDPC/B.hs
+++ EDPC/B.hs
@@ -1,19 +1,26 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+import Control.Monad
+import Data.Maybe
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Vector.Unboxed as UV
 import Debug.Trace
-type DPTable = [(Int,Int)]
+
+readInt = fst . fromJust . BS.readInteger
+readIntList = map readInt . BS.words
+getInt = readInt <$> BS.getLine
+getIntList = readIntList <$> BS.getLine
 
 main = do
-    [n,k]  <- map read . words <$> getLine
-    hs <- map read . words <$> getLine
-    print $ solve k hs
+    [n,k]  <- getIntList
+    hs <- map fromIntegral <$> getIntList
+    let hs' = UV.fromList hs
+    print $ solve (fromIntegral n) (fromIntegral k) hs'
 
-solve k (h1:hs) = snd . last $ foldl update (zip (replicate (k - 1) (-1) ++ [h1]) (repeat 0)) hs
-
-update :: DPTable -> Int -> DPTable
-update dp h = tail $ dp ++ [foldr (f h) (h, maxBound::Int) dp]
-
-f :: Int -> (Int, Int) -> (Int, Int) -> (Int, Int)
-f h (hi_j, dpi_j) (hi, dpi)
-   | hi_j < 0 = (hi, dpi)
-   | dpi > dpi_j + abs (hi_j - h) = (hi, dpi_j + abs (hi_j - h))
-   | otherwise = (hi, dpi)
+solve n k = flip (UV.!) (n - 1) . UV.constructN n . f 
+        where f :: UV.Vector Int -> UV.Vector Int -> Int
+              f hs dp
+                | UV.null dp = 0
+                | otherwise = minimum $ step k hs dp
+              step k hs dp = map (fm hs dp) [1..min k $ sz dp]
+              fm hs dp i = (UV.!) dp (sz dp - i) + dh hs dp i
+              dh hs dp i = abs ((UV.!) hs (sz dp) - (UV.!) hs (sz dp - i))
+              sz = UV.length
