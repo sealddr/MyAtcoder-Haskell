81f8e518a50a79c225913dc2428b55cdc00d34d1
masakazu.kadoshita <mkadoshita@gmail.com>
2022-05-14 23:47:26 +0900

    ABC251 ProblemA AC

--- ABC25x/ABC251/a.hs
+++ ABC25x/ABC251/a.hs
@@ -1,8 +1,1 @@
-
-main = do
-    s <- getLine
-    let solve s
-          | length s == 1 = replicate 6 s
-          | length s == 2 = replicate 3 s
-          | otherwise = replicate 2 s
-    putStrLn . concat $ solve s
+main = getLine >>= putStrLn . take 6 . concat . repeat
