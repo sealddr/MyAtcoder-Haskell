0bb1b53e5b78216faaba5acad7b02a23255c8bda
masakazu.kadoshita <mkadoshita@gmail.com>
2023-10-01 20:38:14 +0900

    ABC322 Problem C AC

---
+++ ABC32x/ABC322/C.hs
@@ -0,0 +1,18 @@
+import Control.Monad
+import Data.Maybe
+import qualified Data.ByteString.Char8 as BS
+import Data.List
+
+readInt = fst . fromJust . BS.readInteger
+readIntList = map readInt . BS.words
+getInt = readInt <$> BS.getLine
+getIntList = readIntList <$> BS.getLine
+
+main = do
+    [n, m] <- getIntList
+    a_s <- getIntList
+    let ans = fst $ foldl f ([], a_s) [1..n]
+    mapM_ print $ reverse ans
+        where f :: ([Integer], [Integer]) -> Integer -> ([Integer], [Integer])
+              f (ans, a:a_s) i = if a < i then f (ans, a_s) i else ((a-i):ans,a:a_s)
+              f (ans, []) _ = (ans, [])
