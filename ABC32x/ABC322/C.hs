9d11f1bce140979b8e8cb1f6693eaca8e881808e
masakazu.kadoshita <mkadoshita@gmail.com>
2023-10-01 20:43:23 +0900

    ABC322 Problem C AC minor changes

--- ABC32x/ABC322/C.hs
+++ ABC32x/ABC322/C.hs
@@ -11,8 +11,8 @@ getIntList = readIntList <$> BS.getLine
 main = do
     [n, m] <- getIntList
     a_s <- getIntList
-    let ans = fst $ foldl f ([], a_s) [1..n]
-    mapM_ print $ reverse ans
+    let ans = reverse . fst $ foldl f ([], a_s) [1..n]
+    mapM_ print ans
         where f :: ([Integer], [Integer]) -> Integer -> ([Integer], [Integer])
               f (ans, a:a_s) i = if a < i then f (ans, a_s) i else ((a-i):ans,a:a_s)
               f (ans, []) _ = (ans, [])
