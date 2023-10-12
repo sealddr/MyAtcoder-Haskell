bde429c73d985b17ae1847bc84126247736fea49
masakazu.kadoshita <mkadoshita@gmail.com>
2023-10-13 01:28:51 +0900

    ABC100 Problem C AC

---
+++ ABC10x/ABC100/C.hs
@@ -0,0 +1,14 @@
+import Control.Monad
+import Data.Maybe
+import qualified Data.ByteString.Char8 as BS
+
+readInt = fst . fromJust . BS.readInteger
+readIntList = map readInt . BS.words
+getInt = readInt <$> BS.getLine
+getIntList = readIntList <$> BS.getLine
+
+main = do
+    n <- getInt
+    as <- getIntList
+    print . sum $ map f as
+        where f = length . takeWhile even . iterate (`div` 2)
